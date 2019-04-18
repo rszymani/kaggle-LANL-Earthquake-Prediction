3be24d2bb15206ddacda057193d413b2cb34c230
Rafal Szymaniuk <rszymani@gmail.com>
2019-04-18 20:34:23 +0000

    updated features

--- RandomSearch.py
+++ RandomSearch.py
@@ -35,83 +35,88 @@ def add_statistics(seg_id,feat_name,X,xc,ws=""):
     X.loc[seg_id,feat_name + "_kurt"+ws] = xc.kurt()
     X.loc[seg_id,feat_name + "_skew"+ws] = xc.skew()
     
+def add_quantiles(seg_id,feat_name,X,xc,ws=""):
+    X.loc[seg_id,feat_name + "_25_quantile"+ws] = xc.quantile(0.25)
+    X.loc[seg_id,feat_name + "_50_quantile"+ws] = xc.quantile(0.50)
+    X.loc[seg_id,feat_name + "_75_quantile"+ws] = xc.quantile(0.75)
+    X.loc[seg_id,feat_name + "_1_quantile"+ws] = xc.quantile(0.01)
+    X.loc[seg_id,feat_name + "_99_quantile"+ws] = xc.quantile(0.99)
+    X.loc[seg_id,feat_name + "_5_quantile"+ws] = xc.quantile(0.05)
+    X.loc[seg_id,feat_name + "_95_quantile"+ws] = xc.quantile(0.95)
+    
 def create_features(seg_id,seg, X):
     xc = seg["acoustic_data"]
     
     fftxc = fft(xc)
     X.loc[seg_id,"fft_mean"] = np.abs(fftxc).mean()
-    X.loc[seg_id,"fft_max"] = np.abs(fftxc).max()
+    X.loc[seg_id,"fft_var"] = np.abs(fftxc).var()
     X.loc[seg_id,"fft_min"] = np.abs(fftxc).min()
-    
-    X.loc[seg_id,"25_quantile"] = xc.quantile(0.25)
-    X.loc[seg_id,"50_quantile"] = xc.quantile(0.50)
-    X.loc[seg_id,"75_quantile"] = xc.quantile(0.75)
-    X.loc[seg_id,"1_quantile"] = xc.quantile(0.01)
-    X.loc[seg_id,"99_quantile"] = xc.quantile(0.99)
+    X.loc[seg_id,"fft_max"] = np.abs(fftxc).max()
     
     xc_splitted = np.array_split(xc, 4)
-    X.loc[seg_id,"1_part_mean"] = xc_splitted[0].mean()
-    X.loc[seg_id,"2_part_mean"] = xc_splitted[1].mean()
-    X.loc[seg_id,"3_part_mean"] = xc_splitted[2].mean()
-    X.loc[seg_id,"4_part_mean"] = xc_splitted[3].mean()
-    X.loc[seg_id,"1_part_var"] = xc_splitted[0].var()
-    X.loc[seg_id,"2_part_var"] = xc_splitted[1].var()
-    X.loc[seg_id,"3_part_var"] = xc_splitted[2].var()
-    X.loc[seg_id,"4_part_var"] = xc_splitted[3].var()
+   
+    add_statistics(seg_id,"1_part",X,xc_splitted[0])
+    add_statistics(seg_id,"2_part",X,xc_splitted[1])
+    add_statistics(seg_id,"3_part",X,xc_splitted[2])
+    add_statistics(seg_id,"4_part",X,xc_splitted[3])
     
-    #add_statistics(seg_id,"1_part",X,xc_splitted[0])
-    #add_statistics(seg_id,"2_part",X,xc_splitted[1])
-    #add_statistics(seg_id,"3_part",X,xc_splitted[2])
-    #add_statistics(seg_id,"4_part",X,xc_splitted[3])
-    X.loc[seg_id,"avg"] = xc.mean()
-    X.loc[seg_id,"max"] = xc.max()
-    X.loc[seg_id,"min"] = xc.min()
-    X.loc[seg_id,"skew"] = xc.skew()
-    X.loc[seg_id,"kurt"] = xc.kurt()
-    X.loc[seg_id,"var"] = xc.var()
+    add_quantiles(seg_id,"1_part",X,xc_splitted[0])
+    add_quantiles(seg_id,"2_part",X,xc_splitted[1])
+    add_quantiles(seg_id,"3_part",X,xc_splitted[2])
+    add_quantiles(seg_id,"4_part",X,xc_splitted[3])
     
-    #add_statistics(seg_id,"seg",X,xc)
+    fft_p1 = fft(xc_splitted[0])
+    X.loc[seg_id,"fft_mean_p1"] = np.abs(fft_p1).mean()
+    X.loc[seg_id,"fft_var_p1"] = np.abs(fft_p1).var()
+    X.loc[seg_id,"fft_min_p1"] = np.abs(fft_p1).min()
+    X.loc[seg_id,"fft_max_p1"] = np.abs(fft_p1).max()
     
-    #window_sizes = [20,100,500,1000]
-    #for window_size in window_sizes:
-    #    xc_rolled = xc.rolling(window_size)
-    #    xc_rolled_mean = xc_rolled.mean().dropna()
-    #    xc_rolled_var = xc_rolled.var().dropna()
-    #    ws = "_"+str(window_size)
-    #    
-    #    add_statistics(seg_id,"rollingMean",X,xc_rolled_mean,ws=ws)
-    #    add_statistics(seg_id,"rollingVar",X,xc_rolled_var,ws=ws)
+    fft_p2 = fft(xc_splitted[1])
+    X.loc[seg_id,"fft_mean_p2"] = np.abs(fft_p2).mean()
+    X.loc[seg_id,"fft_var_p2"] = np.abs(fft_p2).var()
+    X.loc[seg_id,"fft_min_p2"] = np.abs(fft_p2).min()
+    X.loc[seg_id,"fft_max_p2"] = np.abs(fft_p2).max()
+    
+    fft_p3 = fft(xc_splitted[2])
+    X.loc[seg_id,"fft_mean_p3"] = np.abs(fft_p3).mean()
+    X.loc[seg_id,"fft_var_p3"] = np.abs(fft_p3).var()
+    X.loc[seg_id,"fft_min_p3"] = np.abs(fft_p3).min()
+    X.loc[seg_id,"fft_max_p3"] = np.abs(fft_p3).max()
+    
+    fft_p4 = fft(xc_splitted[3])
+    X.loc[seg_id,"fft_mean_p4"] = np.abs(fft_p4).mean()
+    X.loc[seg_id,"fft_var_p4"] = np.abs(fft_p4).var()
+    X.loc[seg_id,"fft_min_p4"] = np.abs(fft_p4).min()
+    X.loc[seg_id,"fft_max_p4"] = np.abs(fft_p4).max()
+    
+    add_statistics(seg_id,"seg",X,xc)
+    add_quantiles(seg_id,"seg",X,xc)
+    
+    window_sizes = [20,200,1000]
+    for window_size in window_sizes:
+        xc_rolled = xc.rolling(window_size)
+        xc_rolled_mean = xc_rolled.mean().dropna()
+        xc_rolled_var = xc_rolled.var().dropna()
+        xc_rolled_kurt = xc_rolled.kurt().dropna()
+        xc_rolled_skew = xc_rolled.skew().dropna()
+        ws = "_"+str(window_size)
         
-        #X.loc[seg_id,"rolling_mean_"+ws] = xc_rolled_mean.mean()
-        #X.loc[seg_id,"rolling_max_"+ ws] = xc_rolled_mean.max()
-        #X.loc[seg_id,"rolling_min_"+ws] = xc_rolled_mean.min()
-        #X.loc[seg_id,"rolling_skew_"+ws] = xc_rolled_mean.skew()
-        #X.loc[seg_id,"rolling_kurt_"+ws] = xc_rolled_mean.kurt()
-        #X.loc[seg_id,"rolling_var_"+ws] = xc_rolled_mean.var()
-        #
-        #X.loc[seg_id,"rollingvar_mean_"+ws] = xc_rolled_var.mean()
-        #X.loc[seg_id,"rollingvar_max_"+ ws] = xc_rolled_var.max()
-        #X.loc[seg_id,"rollingvar_min_"+ws] = xc_rolled_var.min()
-        #X.loc[seg_id,"rollingvar_skew_"+ws] = xc_rolled_var.skew()
-        #X.loc[seg_id,"rollingvar_kurt_"+ws] = xc_rolled_var.kurt()
-        #X.loc[seg_id,"rollingvar_var_"+ws] = xc_rolled_var.var()
+        add_statistics(seg_id,"rollingMean",X,xc_rolled_mean,ws=ws)
+        add_statistics(seg_id,"rollingVar",X,xc_rolled_var,ws=ws)
+        add_statistics(seg_id,"rollingKurt",X,xc_rolled_skew,ws=ws)
+        add_statistics(seg_id,"rollingSkew",X,xc_rolled_kurt,ws=ws)
         
-for seg_id in tqdm(range(segments)):
+        add_quantiles(seg_id,"rollingMean",X,xc_rolled_mean,ws=ws)
+        add_quantiles(seg_id,"rollingVar",X,xc_rolled_var,ws=ws)
+        add_quantiles(seg_id,"rollingKurt",X,xc_rolled_skew,ws=ws)
+        add_quantiles(seg_id,"rollingSkew",X,xc_rolled_kurt,ws=ws)
+
+for seg_id in range(segments):
     seg = train_df.iloc[seg_id*rows:seg_id*rows+rows]
     create_features(seg_id, seg,train_X)
     train_y.loc[seg_id, 'time_to_failure'] = seg['time_to_failure'].values[-1]
-    
-def load_test():
-    signals = []
-    segment_names = [file for file in os.listdir("../input") if file.startswith("seg")]
-    test_df = pd.DataFrame(index=segment_names, dtype=np.float64)
-    test_df.index = test_df.index.str[:-4]
-    for file in tqdm(segment_names):
-        seg_id = file[:-4]
-        segment = pd.read_csv(os.path.join("../input",file),dtype={'acoustic_data': np.int16})
-        create_features(seg_id,segment,test_df)
-    return test_df
-test_df = load_test()
+    if seg_id % 500 == 0:
+        print("iteration {}".format(seg_id))
 
 
 def cross_validate(train_X,train_y,params):
@@ -138,30 +143,32 @@ def random_search(train_X,train_y,nr_iterations):
     best_score = np.Infinity
     best_params = None
     for it in range(nr_iterations):
+        print("iteration {}/{}".format(it,nr_iterations))
         params = {
             'boosting_type': rand.choice(['gbdt','rf']),
             'objective': 'regression',
             'min_data_in_leaf': randint(1,100), 
-            'num_leaves': randint(1,100),
-            'max_depth': rand.choice([-1,randint(1,20)]),
+            'num_leaves': randint(2,100),
+            'max_depth': rand.choice([-1,randint(2,20)]),
             'learning_rate': rand.uniform(0.0005,0.007),
             'feature_fraction': rand.uniform(0.5,0.98),
             'bagging_fraction': rand.uniform(0.5,0.98),
             'bagging_freq': randint(1,20),
-            'verbose': 1,
             'lambda_l1':rand.uniform(0,0.01),
             'lambda_l2':rand.uniform(0,0.01),
-            'metric':'mae'
+            'metric':'mae',
+            'verbose':-1
         }
         train_score = cross_validate(train_X,train_y,params)
+        print("params = {}".format(params))
         if train_score < best_score:
+            print("updated score from {} to {}".format(best_score,train_score))
             best_params = params
             best_score = train_score
-            print("updated score from {} to {}".format(best_score,train_score))
         else:
             print("score = {}, leaving {}".format(train_score,best_score))
          
 
     print("Best score achieved {} for params\n {}".format(best_score,best_params))
 
-random_search(train_X,train_y,100)
+random_search(train_X,train_y,2000)
